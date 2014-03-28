0fd00ad0d2e2fcf1dd01a3e27a88f3f7f4210eb1
Ivan <chaosware@gmail.com>
2014-03-28 23:39:32 +0100

    nuevo servicio de utilidades

---
+++ src/services/UtilsService.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Aleksandr on 28/03/14.
+ */
+var utilsServiceModule = angular.module('utilsServiceModule', []);
+
+utilsServiceModule.factory('UtilsService', function(filterFilter){
+    var UtilsService = {};
+
+    UtilsService.createHeaders = function(headers, notEditableFields, hiddenColumns){
+        var classHeaders = [];
+        angular.forEach(headers, function(value, key){
+            classHeaders.push({
+                name: value,
+                isEditable: !_.contains(notEditableFields, value),
+                isHidden: _.contains(hiddenColumns, value)
+            });
+        });
+        return classHeaders;
+    };
+
+    UtilsService.filterData = function(filterParams, scopedData){
+        if(_.isString(filterParams)){
+            return filterParams.length > 0 ? filterFilter(scopedData, filterParams) : scopedData;
+        } else if(!_.isEmpty(filterParams)){
+            return filterFilter(scopedData, filterParams);
+        } else {
+            return scopedData;
+        }
+    };
+
+    UtilsService.setHiddenColumns = function(headers, hiddenColumns){
+        angular.forEach(headers, function(value, key){
+            value.isHidden = _.contains(hiddenColumns, value.name);
+        });
+    };
+
+    return UtilsService;
+});
