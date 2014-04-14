706a5619daf204fe62038a842203b7fd000895ad
Ivan <chaosware@gmail.com>
2014-04-14 23:26:14 +0200

    Correcciones en animaciones -Por ahora se dejan los elementos de la tabla con la animación itvFade ya que la itvSlide no funciona bien a no ser que se recubra con un <div> extra todos los elementos a deslizar (así si funciona). Pero eso hace que los estilos de la tabla no se integren bien con el panel por lo que habrá que revisarlo e implementarlos a mano.

--- src/animations/itvAnimations.js
+++ src/animations/itvAnimations.js
@@ -10,7 +10,7 @@ var itvAnimationsModule = angular.module('itvAnimations', []);
 
 /**
  * @ngdoc animation
- * @name itvSlideTable
+ * @name itvSlide
  * @description
  *
  * Realiza una animación de deslizamiento arriba y abajo cuando se añade y elimina
@@ -19,17 +19,31 @@ var itvAnimationsModule = angular.module('itvAnimations', []);
  * se añada la clase '.ngHide' dado que si no se ocultaría el elemento antes de la animación.
  * Se utiliza 'removeClass' por el mismo motivo, realizar la animación una vez se ha quitado
  * la clase '.ngHide' del elemento.
+ * Es necesario guardar la altura del elemento para realizar correctamente el deslizamiento
+ * al mostrar.
+ * Durante la animación de deslizar hacia arriba (ocultar) se añade la clase 'overflowHidden'
+ * para que funcione correctamente, pero como da problemas si se deja de continuo tras
+ * realizar la animación de deslizar hacia abajo (mostrar) se vuelve a quitar y para que no
+ * se solape con la transición se le establece un timeout ligeramente superior a la duración
+ * definida.
  */
-itvAnimationsModule.animation('.itvSlideTable', function(){
+itvAnimationsModule.animation('.itvSlide', function($timeout){
+    var height = 0;
     return {
         beforeAddClass: function(element, className){
+            console.log('beforeaddclass con className: ' + className);
+           height = element.height();
             if(className == 'ng-hide'){
-                TweenMax.to(element, 1, {opacity: 0, display: 'none'});
+                element.addClass('overflowHidden');
+                TweenMax.to(element, 1, {css: {height: 0}});
             }
         },
         removeClass: function(element, className){
             if(className == 'ng-hide'){
-                TweenMax.to(element, 1, {opacity: 1, display: 'table'});
+                TweenMax.to(element, 1, {css: {height: height}});
+                $timeout(function(){
+                    element.removeClass('overflowHidden');
+                }, 1100);
             }
         }
     }
@@ -37,16 +51,15 @@ itvAnimationsModule.animation('.itvSlideTable', function(){
 
 /**
  * @ngdoc animation
- * @name itvSlide
+ * @name itvFade
  * @description
  *
- * Realiza una animación de deslizamiento arriba y abajo cuando se añade y elimina
+ * Realiza una animación de 'fade in-out' cuando se añade y elimina
  * la clase '.ngHide' de un elemento.
- * Similar a la animación 'itvSlideTable', con la única diferencia de que en la
- * animación de 'removeClass' utiliza el estilo 'display: 'block''. Si se utilizase
- * esta animación para la tabla, no se visualizaría correctamente.
+ * A la hora de realizar la animación de volver a mostrar el elemento es
+ * necesario comprobar si es una tabla para poner el valor correcto en la propiedad 'display'
  */
-itvAnimationsModule.animation('.itvSlide', function(){
+itvAnimationsModule.animation('.itvFade', function(){
     return {
         beforeAddClass: function(element, className){
             if(className == 'ng-hide'){
@@ -55,7 +68,8 @@ itvAnimationsModule.animation('.itvSlide', function(){
         },
         removeClass: function(element, className){
             if(className == 'ng-hide'){
-                TweenMax.to(element, 1, {opacity: 1, display: 'block'});
+                var displayMode = (element[0].getAttribute('class')).indexOf('table') != -1 ? 'table' : 'block';
+                TweenMax.to(element, 1, {opacity: 1, display: displayMode});
             }
         }
     }
